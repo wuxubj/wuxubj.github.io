7493b2c0e384ba52779f309bc7644c1c5685753c
wuxubj <742745426@qq.com>
2016-08-12 14:52:58 +0800

    Site updated: 2016-08-12 14:52:47

---
+++ js/myscript/guestbook.js
@@ -0,0 +1,13 @@
+        var ap3 = new APlayer({
+            element: document.getElementById('player3'),
+            narrow: false,
+            autoplay: false,
+            showlrc: false,
+            music: {
+                title: '生活不止眼前的苟且',
+                author: '许巍',
+                url: 'http://o9w8f1xrl.bkt.clouddn.com/APlayer/audio/shenghuobuzhiyanqiandegouqie.mp3',
+                pic: 'http://o9w8f1xrl.bkt.clouddn.com/APlayer/audio/shenghuobuzhiyanqiandegouqie.jpg'
+            }
+        });
+        ap3.init();
