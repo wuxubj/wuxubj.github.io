5032a08abae976af2df91422a2279de39bf2c49a
wuxubj <742745426@qq.com>
2017-05-03 11:19:29 +0800

    20170503

--- js/myscript/guestbook.js
+++ js/myscript/guestbook.js
@@ -6,8 +6,8 @@
             music: {
                 title: '生活不止眼前的苟且',
                 author: '许巍',
-                url: 'http://o9w8f1xrl.bkt.clouddn.com/APlayer/audio/shenghuobuzhiyanqiandegouqie.mp3',
-                pic: 'http://o9w8f1xrl.bkt.clouddn.com/APlayer/audio/shenghuobuzhiyanqiandegouqie.jpg'
+                url: 'http://media.wuxubj.cn/%E7%94%9F%E6%B4%BB%E4%B8%8D%E6%AD%A2%E7%9C%BC%E5%89%8D%E7%9A%84%E8%8B%9F%E4%B8%94.mp3',
+                pic: 'http://media.wuxubj.cn/%E7%94%9F%E6%B4%BB%E4%B8%8D%E6%AD%A2%E7%9C%BC%E5%89%8D%E7%9A%84%E8%8B%9F%E4%B8%94.jpg'
             }
         });
         ap3.init();
